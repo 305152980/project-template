f83d0c240895faca3dee458326575ed2de4bec51
lijie <lj19102837815@163.com>
2022-11-28 13:01:39 +0800

    日常提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+// 导入 CSS 样式重置库。
+import 'normalize.css'
 
 // 导入自定义公共样式。
 import '@/assets/styles/index.scss'
+
+import App from './App.vue'
+
+// 导入 VueRouter 模块。
+import router from '@/router/index.js'
+// 添加路由权限控制。
+import '@/router/routerNavGuard.js'
 
 // 导入 Vuex 模块。
 import store from '@/store/index.js'
@@ -10,6 +19,7 @@ import store from '@/store/index.js'
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/constantRoutes.js
@@ -0,0 +1,4 @@
+import moduleA from '@/router/modules/moduleA.js'
+import moduleB from '@/router/modules/moduleB.js'
+
+export default [...moduleA, ...moduleB]
