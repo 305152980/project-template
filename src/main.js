692d70fe4fa7ddfcd57ea7e4e8582e17913a3713
lijie <lj19102837815@163.com>
2022-11-28 10:32:52 +0800

    日常提交

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import App from './App.vue'
 // 导入自定义公共样式。
 import '@/assets/styles/index.scss'
 
+// 导入 Vuex 模块。
+import store from '@/store/index.js'
+
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import moduleA from '@/store/modules/moduleA.js'
+import moduleB from '@/store/modules/moduleB.js'
+import getters from '@/store/getters.js'
+
+// 全局注册 Vuex 状态管理模式。
+Vue.use(Vuex)
+
+// 实例化 Vuex 中的 Store 对象。
+const store = new Vuex.Store({
+  modules: {
+    moduleA,
+    moduleB
+  },
+  getters
+})
+
+export default store
