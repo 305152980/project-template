2491538ca4cd137f594bf48b5fd3701f8d7cd3f6
lijie <lj19102837815@163.com>
2022-12-01 22:00:59 +0800

    日常提交

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import 'normalize.css'
 // 导入自定义公共样式。
 import '@/assets/styles/index.scss'
 
+// 按需导入 ElementUI 组件库。
+import '@/components/elementui/index.js'
+
 import App from './App.vue'
 
 // 导入 VueRouter 模块。
