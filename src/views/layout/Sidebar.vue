d4156b442937a7c83298f47370a860b9e19b651f
lijie <lj19102837815@163.com>
2023-01-01 02:37:26 +0800

    日常提交

---
+++ src/views/layout/Sidebar.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="sidebar-container">
+    <div class="sidebar-header">侧边菜单栏</div>
+    <el-menu router :default-active="defaultActive" background-color="#d3dce6">
+      <!-- 一级菜单。 -->
+      <template v-for="(item_1, index_1) in sidebarMenuTree">
+        <el-menu-item v-if="item_1.address" :key="`${index_1}key`" :index="item_1.address">
+          <template slot="title">
+            <i :class="item_1.icon"></i>
+            <span>{{ item_1.title }}</span>
+          </template>
+        </el-menu-item>
+        <el-submenu v-else :key="`${index_1}key`" :index="item_1.address">
+          <template slot="title">
+            <i :class="item_1.icon"></i>
+            <span>{{ item_1.title }}</span>
+          </template>
+          <!-- 二级菜单。 -->
+          <template v-for="(item_2, index_2) in item_1.children">
+            <el-menu-item v-if="item_2.address" :key="`${index_2}key`" :index="item_2.address">
+              {{ item_2.title }}
+            </el-menu-item>
+            <el-submenu v-else :key="`${index_2}key`" :index="item_2.address">
+              <template slot="title">{{ item_2.title }}</template>
+              <!-- 三级菜单。 -->
+              <template v-for="(item_3, index_3) in item_2.children">
+                <el-menu-item :key="`${index_3}key`" :index="item_3.address">
+                  {{ item_3.title }}
+                </el-menu-item>
+              </template>
+            </el-submenu>
+          </template>
+        </el-submenu>
+      </template>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  data() {
+    return {
+      defaultActive: '',
+      sidebarMenuTree: [
+        {
+          icon: 'el-icon-location', // 只有第一级菜单有 icon 属性。
+          title: '模块一',
+          address: '', // address 为空，代表为目录；address 非空，代表为页面。
+          children: [
+            {
+              title: '首页',
+              address: '/home',
+              children: []
+            }
+          ]
+        },
+        {
+          icon: 'el-icon-menu',
+          title: '模块二',
+          address: '/111111',
+          children: []
+        }
+      ]
+    }
+  },
+  watch: {
+    $route: {
+      handler(to, from) {
+        this.defaultActive = to.fullPath
+      },
+      immediate: true
+    }
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebar-container {
+  height: 100%;
+  .sidebar-header {
+    height: 60px;
+    font-size: 28px;
+    font-weight: 700;
+    line-height: 60px;
+    text-align: center;
+  }
+  .el-menu {
+    height: calc(100% - 80px);
+    margin-bottom: 20px;
+    border-right: none;
+    overflow-y: auto;
+  }
+}
+</style>
