3365936459a2717da0f3ff4ecf907d171b997ad6
lijie <lj19102837815@163.com>
2023-01-01 02:32:28 +0800

    日常提交

---
+++ src/views/layout/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar-container">navbar</div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar-container {
+  height: 100%;
+}
+</style>
