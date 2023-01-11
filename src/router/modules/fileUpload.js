1e00e49ca18e1e1dc171ffc31784fa6e1e198d5f
lijie <lj19102837815@163.com>
2023-01-11 23:53:45 +0800

    日常提交

--- src/router/modules/fileUpload.js
+++
@@ -1,9 +0,0 @@
-const FileUpload = () => import('@/views/fileUpload/index.vue')
-
-export default [
-  {
-    path: '/fileUpload',
-    name: 'fileUpload',
-    component: FileUpload
-  }
-]
---
+++ src/views/fileUploadDownload/fileUpload/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="fileUpload-container">fileUpload</div>
+</template>
+
+<script>
+export default {
+  name: 'FileUpload',
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.fileUpload-container {
+}
+</style>
