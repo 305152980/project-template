1de4a652675088efc15d8b34c6c84f6e785ce920
lijie <lj19102837815@163.com>
2023-01-12 23:37:11 +0800

    日常提交

---
+++ src/views/echarts/mapJson/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mapJson-container">
+    <div id="mapBox" style="width: 100%; height: 100%"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MapJson',
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.mapJson-container {
+  height: 100%;
+}
+</style>
