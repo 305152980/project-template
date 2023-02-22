40afdb50b8fd5196d0db78c703f8b50b337d5d69
lijie <lj19102837815@163.com>
2023-02-22 20:02:33 +0800

    日常提交

--- src/router/router-nav-guard.js
+++ src/router/router-nav-guard.js
@@ -1,5 +1,14 @@
 import router from '@/router/index.js'
 
+// 引入进度条。
+import NProgress from 'nprogress'
+
+// 引入进度条样式。
+import 'nprogress/nprogress.css'
+
+// 去除进度条右上角的小圆圈。
+NProgress.configure({ showSpinner: false })
+
 // 全局前置路由导航守卫。
 router.beforeEach((to, from, next) => {
   next()
