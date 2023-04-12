96bdd92fe61a452a8fab58cabea74c2fe78b836f
lijie <lj19102837815@163.com>
2023-04-12 19:13:57 +0800

    日常提交

--- src/views/dev-ing/demo-one/index.vue
+++ src/views/dev-ing/demo-one/index.vue
@@ -1,57 +1,20 @@
 <template>
-  <div class="demo-one-container">
-    <div id="baduMapBox"></div>
-  </div>
+  <div class="demo-one-container">demo-one</div>
 </template>
 
 <script>
 export default {
   name: 'DemoOne',
   data() {
-    return {
-      baduMap: null
-    }
+    return {}
   },
   created() {},
-  mounted() {
-    this.initBaduMap()
-  },
-  methods: {
-    initBaduMap() {
-      // 创建地图实例。
-      // eslint-disable-next-line no-undef
-      this.baduMap = new BMapGL.Map('baduMapBox')
-      // 设置中心点坐标。
-      // eslint-disable-next-line no-undef
-      var point = new BMapGL.Point(108.924, 34.541)
-      // 地图初始化，同时设置地图展示级别。(3 - 19)
-      this.baduMap.centerAndZoom(point, 4)
-      // 开启鼠标滚轮缩放。
-      this.baduMap.enableScrollWheelZoom(true)
-      // 为地图添加控件。
-      // eslint-disable-next-line no-undef
-      this.baduMap.addControl(new BMapGL.LocationControl())
-      // eslint-disable-next-line no-undef
-      this.baduMap.addControl(new BMapGL.ScaleControl())
-      // eslint-disable-next-line no-undef
-      this.baduMap.addControl(new BMapGL.ZoomControl())
-    }
-  }
+  mounted() {},
+  methods: {}
 }
 </script>
 
 <style lang="scss" scoped>
 .demo-one-container {
-  position: relative;
-  height: 100%;
-  #baduMapBox {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 600px;
-    height: 450px;
-    border: 1px #000000 solid;
-  }
 }
 </style>
